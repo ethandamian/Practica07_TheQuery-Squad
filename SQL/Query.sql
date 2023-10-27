d787879f270f7923ae7751cf9bb0d67b67dc0039
alanlopez1 <alanlopez@ciencias.unam.mx>
2023-10-26 21:29:37 -0600

    Queries upddated

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -4,6 +4,15 @@
 SELECT *
 FROM Veterinario
 WHERE LEFT(nombre, 1) = 'C';
--- iv. Animales cuya alimentación sea carnivoro.
 
+-- ii. Visitantes que hayan nacido en el mes de Junio.
+SELECT * FROM Visitante WHERE MONTH(FechaNacimiento) = 6;
+
+-- iii. Alimentos cuya fecha de caducidad este entre el 1 de enero del 2023 y del 18 de octubre del 2023.
+SELECT * FROM Alimento WHERE FechaCaducidad BETWEEN '2023-01-01' AND '2023-10-18';
+
+-- iv. Animales cuya alimentación sea carnivoro.
 SELECT IDAnimal, NombreAnimal, Alimentacion FROM Animal WHERE Alimentacion = 'Carnivoro';
+
+-- v. Todos los visitantes registrados en el zoologico
+SELECT * FROM Visitante;
