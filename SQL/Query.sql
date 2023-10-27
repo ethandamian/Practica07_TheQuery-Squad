97872c5302797f78a8236c0bcba45ce499fac793
ethandamian <ethandamian@ciencias.unam.mx>
2023-10-26 19:40:32 -0600

    Agregue una consulta en Query y el DML para Atender

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -1,1 +1,8 @@
 -- Aqui deben ir las consultas que piden en la practica, pueden hacer pruebas 
+
+
+
+
+
+-- iv. Animales cuya alimentación sea carnivoro.
+SELECT idAnimal,nombreAnimal,Alimentacion FROM Animal WHERE alimentacion = 'Carnivoro';
